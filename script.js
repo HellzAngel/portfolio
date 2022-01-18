cc233cd3d7f79e7f4dc058efa3d6d4c5c9f0fc42
HellzAngel <itzmeanuhere01@gmail.com>
2022-01-18 15:11:18 +0530

    addede

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ let nav = document.querySelector("nav");
 let scrollBtn = document.querySelector(".scroll-button a");
 
 let val;
+window.onload = function () {
+  Particles.init({
+    selector: ".background"
+  });
+};
+var particles = Particles.init({
+  selector: ".background",
+  color: ["#03dac6", "#ff0266", "#000000"],
+  connectParticles: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      options: {
+        color: ["#faebd7", "#03dac6", "#ff0266"],
+        maxParticles: 43,
+        connectParticles: false
+      }
+    }
+  ]
+});
 
 window.onscroll = function() {
   if(document.documentElement.scrollTop > 20){
